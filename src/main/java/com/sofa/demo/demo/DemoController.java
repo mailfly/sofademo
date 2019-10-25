d8e3cba09cf3dad43b8519dc21265ec31ab60b33
林三福 <linsanfu@ppdai.com>
2019-10-25 14:30:24 +0800

    sofademo

---
+++ src/main/java/com/sofa/demo/demo/DemoController.java
@@ -0,0 +1,26 @@
+package com.sofa.demo.demo;
+
+import com.alipay.sofa.runtime.api.annotation.SofaReference;
+import com.alipay.sofa.runtime.api.annotation.SofaReferenceBinding;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DemoController {
+
+    //directUrl = http://www.baidu.cn is 302, change to http://www.tianya.cn is work fine
+    @SofaReference(jvmFirst = false, binding = @SofaReferenceBinding(bindingType = "rest", directUrl = "http://www.baidu.cn"))
+    private SimpleApi simpleApi;
+
+    @RequestMapping(value = "/rest", method = RequestMethod.GET)
+    public String rest() {
+        return "rest" + simpleApi.post();
+    }
+
+    @RequestMapping(value = "/hs", method = RequestMethod.GET)
+    public String hs() {
+        return "OK" ;
+    }
+}
---
+++ src/main/java/com/sofa/demo/demo/SimpleApi.java
@@ -0,0 +1,17 @@
+package com.sofa.demo.demo;
+
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public interface SimpleApi {
+
+    @GET
+    String post();
+}
